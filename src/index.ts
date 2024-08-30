b2eb0476f63d034b1873ab74c73229abe3a8d74e
zahtec <32808999+zahtec@users.noreply.github.com>
2024-08-30 12:50:45 -0700

    feat: OpenGraph image support

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,47 @@
  * `Env` object can be regenerated with `npm run cf-typegen`.
  */
 
+import { extractOpenGraph } from "@devmehq/open-graph-extractor";
+
 export default {
 	async fetch(request, env, _): Promise<Response> {
 		const url = await env.KV.get(new URL(request.url).pathname.split("/")[1] || "undefined");
 
-		return new Response(undefined, {
-			headers: {
-				Location: url ?? "",
-			},
-			status: url ? 301 : 404,
-		});
+		try {
+			if (!url) throw new Error();
+
+			const html = await fetch(url).then((res) => res.text());
+			const img = (
+				(await extractOpenGraph(html)) as {
+					ogImage?: {
+						url: string;
+						width: string;
+						height: string;
+						type: string;
+					};
+				}
+			).ogImage;
+
+			return new Response(
+				`<html><head>
+                <meta property="og:image" name="og:image" content="${img?.url}" />
+                <meta property="og:image:type" content="${img?.type}" />
+                <meta property="og:image:width" content="${img?.width}" />
+                <meta property="og:image:height" content="${img?.height}" />
+                </head></html>`,
+				{
+					headers: {
+						Location: url,
+					},
+					status: 301,
+				}
+			);
+		} catch {
+			if (url) return Response.redirect(url, 301);
+			else
+				return new Response(undefined, {
+					status: 404,
+				});
+		}
 	},
 } satisfies ExportedHandler<Env>;
